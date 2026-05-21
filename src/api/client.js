5c64c73c2b7dd36c80e9b39ce55cdc21692b7d80
Nayan Sharma <nayan.sharma172005@gmail.com>
2026-05-22 01:39:40 +0530

    Connect frontend to deployed backend

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5000/api/v1' });
+const api = axios.create({ baseURL: 'https://design-n-print.onrender.com/api/v1' });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
